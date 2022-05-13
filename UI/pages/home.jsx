e6a4cf62d7ea443ab092afd179595cd19afb5ce6
Hritwik <triptathi.hritwik@gmail.com>
2022-05-13 18:03:39 +0530

    added most of the work

---
+++ UI/pages/home.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+
+
+const Button = styled.button`
+`;
+
+
+
+function home() {
+
+
+    //console.log(window);
+    return (
+        <div>
+            c,z m m 
+            <Button>skgcgscjhscb</Button>
+        </div>
+    );
+}
+
+export default home;
