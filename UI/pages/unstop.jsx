b72a5b790756b9956ad31984d9314e2ecb5dc9be
Hritwik <triptathi.hritwik@gmail.com>
2022-05-14 23:45:42 +0530

    tried unstoppable domains but no luck

---
+++ UI/pages/unstop.jsx
@@ -0,0 +1,40 @@
+
+
+
+// import {web3modal} from '../connector/web3modal'
+// import Web3 from 'web3'
+// import styled from "styled-components";
+
+
+// const Div = styled.div``;
+// const Button = styled.button``;
+
+
+// function unstop() {
+
+//     const l = async() => {
+//         const provider = await web3modal.connect()
+//     console.log(provider)
+//     }
+    
+
+//     return (
+//         <Div>
+//             Enter
+//             <Button onClick={l}>Unstop</Button>
+//         </Div>
+//     );
+// }
+
+// export default unstop;
+
+
+function unstop() {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default unstop;
---
+++ UI/pages/walletconnect.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+const Div = styled.div``;
+const Button = styled.button``;
+
+const kk = () => {
+
+}
+
+function walletconnect() {
+    return (
+        <Div>
+            <Button onClick={kk}>walletconnect</Button>
+            Enter
+        </Div>
+    );
+}
+
+export default walletconnect;
